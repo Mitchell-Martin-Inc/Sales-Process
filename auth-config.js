9ec50bdf3a546fb2b83c056ebb6eff91a29f4f29
MMI-Tech-Admin <tech.admin@itmmi.com>
2026-07-01 09:44:31 -0400

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -4,8 +4,8 @@ window.playbookAuthConfig = {
   allowedDomains: ["mitchellmartin.com", "itmmi.com"],
   msal: {
     auth: {
-      clientId: "REPLACE_WITH_ENTRA_APP_CLIENT_ID",
-      authority: "https://login.microsoftonline.com/REPLACE_WITH_TENANT_ID",
+      clientId: "22b31d32-ad85-4c7f-9e91-d9233dc1b871",
+      authority: "https://login.microsoftonline.com/7f492253-744d-45d5-81fa-5cff91344686",
       redirectUri: window.location.origin + window.location.pathname,
       postLogoutRedirectUri: window.location.origin + window.location.pathname,
       navigateToLoginRequestUrl: false
